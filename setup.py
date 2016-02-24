21ad4caacd86abc0819df3479546084f355a5bbc
Luke Faraone <lfaraone@dropbox.com>
2016-02-24 18:34:58 +0000

    Don't specify requirements*.txt, instead preferring install_requires/tests_require

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 
-import setuptools
-from distutils.core import setup
+from setuptools import setup
 
 execfile("groupy/version.py")
 
-with open('requirements.txt') as requirements:
-    required = requirements.read().splitlines()
-
 kwargs = {
     "name": "groupy",
-    "version": str(__version__),
+    "version": str(__version__),  # noqa
     "packages": ["groupy"],
     "description": "Python client library for Grouper",
     # TODO(lfaraone): Check whether this is still needed for PyPI.
@@ -20,7 +16,18 @@ kwargs = {
     "author_email": "gary@dropbox.com",
     "maintainer_email": "gary@dropbox.com",
     "license": "Apache-2.0",
-    "install_requires": required,
+    "install_requires": [
+        "clowncar",
+        "tornado>=3.2",
+    ],
+    "setup_requires": [
+        "flake8",
+        "pytest-runner",
+    ],
+    "tests_require": [
+        "pytest>=2.6",
+        "mock>=1.0",
+    ],
     "url": "https://github.com/dropbox/groupy",
     "classifiers": [
         "Programming Language :: Python",
